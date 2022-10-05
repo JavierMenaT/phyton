a1a0de72232eaab200718b2b6f05d4ba86e8c8d7
JavierMenaT <javomena@gmail.com>
2022-10-04 21:20:55 -0500

    conversión de unidades farenheit a celsius

---
+++ 9unitconversion.py
@@ -0,0 +1,12 @@
+"""
+File: Unit Conversion
+Author: Javier Mena
+Class: CSE 110
+Number: 9
+"""
+#get the data
+farenheit = float(input("What is the temperature in Farenheit? "))
+#operate
+celsius = (farenheit - 32) * (5 / 9)
+#show
+print(f"The temperature in Celsius is: {celsius:.1f} degrees")
