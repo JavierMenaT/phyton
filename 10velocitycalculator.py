b6a127bf8f2ae3e7d02eb53688bfd1896be98a26
JavierMenaT <javomena@gmail.com>
2022-10-06 18:45:12 -0500

    last

--- 10velocitycalculator.py
+++ 10velocitycalculator.py
@@ -20,4 +20,4 @@ velocity2=(math.sqrt((m * g ) / c)) * (1 - math.e**((-math.sqrt(m * g * c) / m) 
 print(f"The inner value of c: {c:.3f}")
 print(f"The velocity after {t} seconds is: {velocity:.3f} m/s")
 print(f"The velocity 2 after {t} seconds is: {velocity2:.4f} m/s")
-
+#end
