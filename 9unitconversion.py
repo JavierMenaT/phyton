fdb556f0c6c61b2adf77c34097220dd0ef70e744
JavierMenaT <javomena@gmail.com>
2022-10-05 20:41:15 -0500

    añadir un comentario

--- 9unitconversion.py
+++ 9unitconversion.py
@@ -10,3 +10,4 @@ farenheit = float(input("What is the temperature in Farenheit? "))
 celsius = (farenheit - 32) * (5 / 9)
 #show
 print(f"The temperature in Celsius is: {celsius:.1f} degrees")
+#end of program
