7173f904cea13c579be19660da56ebe2961b6223
JavierMenaT <javomena@gmail.com>
2022-11-06 00:14:26 -0500

    cambios

--- 17comparar.py
+++ 17comparar.py
@@ -5,17 +5,17 @@ Class: CSE 110
 Number: 17
 Date: October, 30 
 """
-
 word = "mormon"
 guess = ""
 cont = 0
 print("Welcome to the word guessing game!\n")
 
 while guess.lower() != word:
-    guess = input("What is your guess? ")
+    guess = input("¿Cuál es tu conjetura? ")
     cont +=1
     if guess.lower() != word:
         print("Your guess is not correct!")
     else:
         print("Congratulations! You guessed it")
         print(f"It toook you: {cont} guesses")
+
--- 18ciclofor2.py
+++ 18ciclofor2.py
@@ -1,4 +1,4 @@
-#Recorrer una palabra y mostrarla verticalmente cambiando la letra ingresada por el usuario si la hay por una mayúscula 
+"""#Recorrer una palabra y mostrarla verticalmente cambiando la letra ingresada por el usuario si la hay por una mayúscula 
 palabra = "commitment"
 letra_favorita = input("¿Cuál es tu letra favorita? ")
 for letra in palabra:
@@ -16,7 +16,7 @@ for letra in palabra:
         print(letra.upper(), end ="")
     else:
         print(letra.lower(), end ="")
-print()
+print()"""
 
 #Recorrer una palabra y mostrarla horizontalmente cambiando la letra ingresada por el usuario si la hay por un guión 
 palabra = "commitment"
